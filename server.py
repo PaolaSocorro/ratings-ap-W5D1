9f41b258d1f35d5b260a81ac55b5bc9ec08c6911
Hackbright Student <info@hackbrightacademy.com>
2015-07-29 18:03:16 -0700

    User login/logout done. User list,sessions and profile.

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -17,12 +17,62 @@ app.secret_key = "ABC"
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
+DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 @app.route('/')
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route('/users')
+def user_list():
+    """Show a list of users"""
+
+    users = User.query.all()
+    return render_template('user_list.html', users=users)
+
+@app.route('/users/<int:id>')
+def user_profile(id):
+    """Show user profile"""
+
+    all_ratings = db.session.query(User, Movie).join(Movie).all()
+    this_user = User.query.filter_by(user_id=id).one()
+    age = this_user.age
+    zipcode = this_user.zipcode
+    print id, age, zipcode
+
+    return render_template("user_profile.html", username=id, age=age, zipcode=zipcode)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_form():
+    """ Login  """
+
+    if request.method == 'POST':
+        username = request.form.get("username") # ""
+        password = request.form.get("password") # ""
+
+        credentials = (username, password)
+
+        session['login_id'] = credentials
+        flash('You were successfully logged in')
+
+        print "LOOK HERE NOW", session
+        return redirect("/")
+
+    else:
+        return render_template("login_form.html")
+
+@app.route('/logout')
+def log_out():
+    """Log out"""
+    # If enough time: hide login/logout button depending if login/logout 
+    if session["login_id"] in session:
+        del session['login_id']
+        flash('You no cool anymore')
+        print "Logged out:" , session
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
